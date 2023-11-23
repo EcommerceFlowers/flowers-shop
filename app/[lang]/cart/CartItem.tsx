9ae3866e8185c04b98053427b7fc0aa463ebae22
tranhuyducseven <tranhuyducseven@gmail.com>
2023-11-23 09:26:45 +0700

    feature/cart: add state global

--- app/[lang]/cart/CartItem.tsx
+++ app/[lang]/cart/CartItem.tsx
@@ -1,8 +1,31 @@
+'use client';
 import Image from 'next/image';
 
 import { MinusIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/solid';
+import { useCartStore } from '@states/cart';
+import { useCallback } from 'react';
 
-export const CartItem: IComponent<TCartItem> = ({ description, image, name, price, quantity }) => {
+export const CartItem: IComponent<TCartItem> = ({
+  description,
+  id,
+  image,
+  name,
+  price,
+  quantity,
+}) => {
+  const { cart, removeFromCart, setCart } = useCartStore();
+  const handleUpdateQuantity = useCallback(
+    (quantity: number) => {
+      const prev = cart.findIndex((item) => item.id === id);
+      const newCart = [...cart];
+      newCart[prev] = {
+        ...newCart[prev],
+        quantity,
+      };
+      setCart(newCart);
+    },
+    [cart, setCart, id]
+  );
   return (
     <div className="flex justify-between gap-8">
       <div>
@@ -15,7 +38,12 @@ export const CartItem: IComponent<TCartItem> = ({ description, image, name, pric
             <p className="text-lg text-gray2">{description}</p>
           </div>
           <span>
-            <TrashIcon width={20} height={20} className="text-gray2" />
+            <TrashIcon
+              onClick={() => removeFromCart(id)}
+              width={20}
+              height={20}
+              className="text-gray2 cursor-pointer hover:opacity-60"
+            />
           </span>
         </div>
 
@@ -23,11 +51,15 @@ export const CartItem: IComponent<TCartItem> = ({ description, image, name, pric
           <div className="flex flex-col gap-2">
             <span className="text-lg text-gray2">Unit price: ${price}</span>
             <div className="w-fit border border-orange-400 flex items-center justify-center gap-2 p-2 rounded">
-              <PlusIcon
+              <MinusIcon
                 strokeWidth={2}
                 width={16}
                 height={16}
                 className="text-orange-400 cursor-pointer hover:opacity-60"
+                onClick={() => {
+                  if (quantity === 1) return;
+                  handleUpdateQuantity(quantity - 1);
+                }}
               />
               <span
                 style={{
@@ -36,11 +68,12 @@ export const CartItem: IComponent<TCartItem> = ({ description, image, name, pric
                 className="text-center text-lg text-black">
                 {quantity}
               </span>
-              <MinusIcon
+              <PlusIcon
                 strokeWidth={2}
                 width={16}
                 height={16}
                 className="text-orange-400 cursor-pointer hover:opacity-60"
+                onClick={() => handleUpdateQuantity(quantity + 1)}
               />
             </div>
           </div>
--- app/[lang]/cart/page.tsx
+++ app/[lang]/cart/page.tsx
@@ -1,59 +1,10 @@
 import { cx } from '@utils/tools';
-import { CartItem } from './CartItem';
-
-const MOCK_DATA: TCartItem[] = [
-  {
-    description: 'Sunflower',
-    id: 1,
-    image: '/images/mock/sunflower.png',
-    name: 'Sunflower',
-    price: 10,
-    quantity: 1,
-  },
-  {
-    description: 'Rose',
-    id: 2,
-    image: '/images/mock/sunflower.png',
-    name: 'Rose',
-    price: 20,
-    quantity: 1,
-  },
-];
+import { PageContent } from './content';
 
 const CartPage: TNextPage = async () => {
-  const items = MOCK_DATA;
-  //TODO: use slug to query information
   return (
-    <div className={cx('dark:text-white bg-gray1', 'px-32 py-16')}>
-      <main className="flex flex-wrap">
-        <section className="w-2/3 p-4 ">
-          <div className="bg-white rounded px-10 py-8">
-            <h2 className="text-xl">Your cart</h2>
-            <div className="mt-4 flex flex-col gap-8">
-              {items.map((item) => (
-                <CartItem key={item.id} {...item} />
-              ))}
-            </div>
-          </div>
-        </section>
-        <section className="w-1/3 p-4 ">
-          <div className="flex flex-col bg-white rounded px-10 py-8 text-xl gap-2">
-            <div className="flex justify-between">
-              <span>Subtotal</span>
-              <span>$30</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Shipping</span>
-              <span>$5</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Total</span>
-              <span>$35</span>
-            </div>
-            <button className="mt-2 bg-orange-400 h-12 text-white rounded">Checkout</button>
-          </div>
-        </section>
-      </main>
+    <div className={cx('dark:text-white bg-gray1 min-h-screen', 'px-32 py-16')}>
+      <main className="flex flex-wrap">{<PageContent />}</main>
     </div>
   );
 };
