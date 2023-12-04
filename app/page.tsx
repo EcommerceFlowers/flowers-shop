bcdbdca437cb7f8f3e7129ba69ca2ac14ffca521
HuyWithNY <huyhmopubg@gmail.com>
2023-12-04 13:44:28 +0700

    HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
-import { cx } from '@utils/tools';
-import styles from './styles.module.scss';
-
+import { Header } from '@components/Header';
+import { Thumbnail } from '@components/Thumbnail';
 /**
  *
  * Home page
  */
 export default function HomePage() {
   return (
-    <div className={cx(styles.container, 'dark:text-white')}>
-      <h1 className="text-black text-2xl">Hello from home pagee</h1>
-      <main className={styles.main}>
-        {/**
-         * thumbnail
-         * best seller
-         * latest posts
-         * comments
-         * flower cards, post cards, comment cards
-         */}
-      </main>
+    <div className=" bg-gray-200 ">
+      <Header />
+      <Thumbnail />
     </div>
   );
 }
